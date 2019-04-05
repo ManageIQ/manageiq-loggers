e89dbc57a2a99f5a9f665a40c2b4376daeabfead
Adam Grare <agrare@redhat.com>
2019-04-05 19:37:12 -0400

    Truncate messages

--- lib/manageiq/loggers/journald.rb
+++ lib/manageiq/loggers/journald.rb
@@ -30,23 +30,25 @@ module ManageIQ
 
         message = formatter.call(format_severity(severity), progname, message)
 
-        Systemd::Journal.print(LOG_LEVEL_MAP[severity], message)
+        Systemd::Journal.print(log_level_map[severity], message)
       end
 
       private
 
-      LOG_LEVEL_MAP = {
-        Logger::UNKNOWN => Systemd::Journal::LOG_ALERT,
-        Logger::FATAL   => Systemd::Journal::LOG_CRIT,
-        Logger::ERROR   => Systemd::Journal::LOG_ERR,
-        Logger::WARN    => Systemd::Journal::LOG_WARNING,
-        Logger::INFO    => Systemd::Journal::LOG_INFO,
-        Logger::DEBUG   => Systemd::Journal::LOG_DEBUG,
-      }
+      def log_level_map
+        @log_level_map ||= {
+          Logger::UNKNOWN => Systemd::Journal::LOG_ALERT,
+          Logger::FATAL   => Systemd::Journal::LOG_CRIT,
+          Logger::ERROR   => Systemd::Journal::LOG_ERR,
+          Logger::WARN    => Systemd::Journal::LOG_WARNING,
+          Logger::INFO    => Systemd::Journal::LOG_INFO,
+          Logger::DEBUG   => Systemd::Journal::LOG_DEBUG,
+        }
+      end
 
       class Formatter < Base::Formatter
         def call(severity, progname, msg)
-          msg = prefix_task_id(msg2str(msg))
+          msg = prefix_task_id(msg2str(msg)).truncate(Base::MAX_LOG_LINE_LENGTH)
           "%5s -- %s: %s\n" % [severity, progname, msg]
         end
       end

1f65549aed2439eb0fabe358da1811f5558ca5c8
Madhu Kanoor <mkanoor@redhat.com>
2019-04-30 17:23:13 -0400

    Get the request id from the thread local variable

--- spec/manageiq/container_spec.rb
+++ spec/manageiq/container_spec.rb
@@ -1,6 +1,15 @@
 require 'manageiq/loggers/container'
 
 describe ManageIQ::Loggers::Container::Formatter do
+  let(:request_id) { "123" }
+  let(:request) { double(:request_id => request_id) }
+  before do
+    Thread.current[:current_request] = request
+  end
+  after do
+    Thread.current[:current_request] = nil
+  end
+
   it "stuff" do
     time = Time.now
     result = described_class.new.call("INFO", time, "some_program", "testing 1, 2, 3")
@@ -12,6 +21,7 @@ describe ManageIQ::Loggers::Container::Formatter do
       "pid"        => $PROCESS_ID,
       "service"    => "some_program",
       "tid"        => Thread.current.object_id.to_s(16),
+      "request_id" => request_id
     }.compact
     expect(JSON.parse(result)).to eq(expected)
   end

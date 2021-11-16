3099a2d37fdbe9568ad5348a7d19b55820f2d295
Jason Frey <fryguy9@gmail.com>
2021-11-15 20:55:59 -0500

    Revert "Drop syslog_identifier from journald logger"
    This reverts commit dc4bc158c6ea1e4f0e4823644029588ab14dec66.

--- spec/manageiq/journald_spec.rb
+++ spec/manageiq/journald_spec.rb
@@ -4,13 +4,20 @@ RSpec.describe ManageIQ::Loggers::Journald, :linux do
   let(:logger) { described_class.new }
 
   context "progname" do
-    it "has a progname accessor" do
-      expect(logger).to respond_to(:progname)
-      expect(logger).to respond_to(:progname=)
-    end
-
     it "sets the progname to manageiq by default" do
       expect(logger.progname).to eql('manageiq')
+    end
+  end
+
+  context "syslog_identifier accessor" do
+    it "has a syslog_identifier accessor" do
+      expect(logger).to respond_to(:syslog_identifier)
+      expect(logger).to respond_to(:syslog_identifier=)
+    end
+
+    it "sets the syslog_identifier to the progname by default" do
+      logger = ManageIQ::Loggers::Journald.new(nil, :progname => 'manageiq-test')
+      expect(logger.syslog_identifier).to eql('manageiq-test')
     end
   end
 

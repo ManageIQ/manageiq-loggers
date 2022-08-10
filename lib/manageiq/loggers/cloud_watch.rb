bdda37a885deddd2da28e8d183bd64735830c2e6
Dávid Halász <skateman@skateman.eu>
2022-08-10 18:13:28 +0200

    Fix parameter naming for cloudwatch and added specs

--- lib/manageiq/loggers/cloud_watch.rb
+++ lib/manageiq/loggers/cloud_watch.rb
@@ -7,19 +7,19 @@ module ManageIQ
     class CloudWatch < Base
       NAMESPACE_FILE = "/var/run/secrets/kubernetes.io/serviceaccount/namespace".freeze
 
-      def self.new(*args, access_key_id: nil, secret_access_key: nil, log_group: nil, hostname: nil)
+      def self.new(*args, access_key_id: nil, secret_access_key: nil, log_group: nil, log_stream: nil)
         access_key_id     ||= ENV["CW_AWS_ACCESS_KEY_ID"].presence
         secret_access_key ||= ENV["CW_AWS_SECRET_ACCESS_KEY"].presence
-        log_group_name    ||= ENV["CLOUD_WATCH_LOG_GROUP"].presence
-        log_stream_name   ||= ENV["HOSTNAME"].presence
+        log_group         ||= ENV["CLOUD_WATCH_LOG_GROUP"].presence
+        log_stream        ||= ENV["HOSTNAME"].presence
 
         container_logger = ManageIQ::Loggers::Container.new
-        return container_logger unless access_key_id && secret_access_key && log_group_name && log_stream_name
+        return container_logger unless access_key_id && secret_access_key && log_group && log_stream
 
         require 'cloudwatchlogger'
 
         creds = {:access_key_id => access_key_id, :secret_access_key => secret_access_key}
-        cloud_watch_logdev = CloudWatchLogger::Client.new(creds, log_group_name, log_stream_name)
+        cloud_watch_logdev = CloudWatchLogger::Client.new(creds, log_group, log_stream)
         super(cloud_watch_logdev).tap { |logger| logger.extend(ActiveSupport::Logger.broadcast(container_logger)) }
       end
 
--- spec/manageiq/cloud_watch_spec.rb
+++ spec/manageiq/cloud_watch_spec.rb
@@ -5,7 +5,7 @@ describe ManageIQ::Loggers::CloudWatch do
     expect(described_class.new).to be_kind_of(ManageIQ::Loggers::Container)
   end
 
-  context "configured" do
+  context "configured via env" do
     around do |example|
       ENV["CW_AWS_ACCESS_KEY_ID"] = "test"
       ENV["CW_AWS_SECRET_ACCESS_KEY"] = "test"
@@ -36,6 +36,34 @@ describe ManageIQ::Loggers::CloudWatch do
       expect(container_logger).to receive(:add).with(1, nil, "Testing 1,2,3")
 
       described_class.new.info("Testing 1,2,3")
+    end
+  end
+
+  context "configured via params" do
+    let(:params) do
+      {
+        access_key_id: 'test',
+        secret_access_key: 'test',
+        log_group: 'test',
+        log_stream: 'test'
+      }
+    end
+
+    before do
+      expect(CloudWatchLogger::Client::AWS_SDK::DeliveryThreadManager).to receive(:new).and_return(double("CloudWatchLogger::Client::AWS_SDK::DeliveryThreadManager", :deliver => nil))
+    end
+
+    it "returns a CloudWatch::Client" do
+      expect(described_class.new(**params)).to be_kind_of(ManageIQ::Loggers::CloudWatch)
+    end
+
+    it "the Container logger also receives the same messages" do
+      container_logger = ManageIQ::Loggers::Container.new
+      expect(ManageIQ::Loggers::Container).to receive(:new).and_return(container_logger)
+
+      expect(container_logger).to receive(:add).with(1, nil, "Testing 1,2,3")
+
+      described_class.new(**params).info("Testing 1,2,3")
     end
   end
 end
